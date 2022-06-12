45e4589dbe9f3125de4965a3f95eec10415733dc
Kim G <xiaole.guo@yahoo.com>
2022-06-12 11:08:00 -0600

    add and configure vcr gem

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -64,3 +64,10 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 end
+VCR.configure do |config|
+  config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
+  config.hook_into :webmock
+  config.configure_rspec_metadata!
+  config.filter_sensitive_data('DONT SHOW MY API KEY') { ENV['mapquest_api_key']}
+  config.filter_sensitive_data('DONT SHOW MY API KEY') { ENV['openweather_api_key']}
+end
