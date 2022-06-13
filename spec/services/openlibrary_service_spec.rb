4b97d9a7f85f45cfa353fa8e5df283efbacdaf9e
Kim G <xiaole.guo@yahoo.com>
2022-06-13 09:34:46 -0600

    setup and complete openlibrary service and tests

---
+++ spec/services/openlibrary_service_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+
+RSpec.describe OpenlibraryService, :vcr do 
+  it 'returns books based on location' do 
+    data = OpenlibraryService.get_books('denver, co')
+
+  	expect(data).to be_a(Hash)
+  	expect(data[:numFound]).to be_an(Integer)
+  	expect(data[:docs]).to be_an(Array)
+  	expect(data[:docs][0][:title]).to eq('Denver, Co')
+    expect(data[:docs][0][:isbn]).to eq(["9780762507849", "0762507845"])
+    expect(data[:docs][0][:publisher]).to eq(["Universal Map Enterprises"])
+  end
+end
