041cc8dc540ba875a036d4bc27525689d4fd2245
Kim G <xiaole.guo@yahoo.com>
2022-06-13 11:07:34 -0600

    indentation fix

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -10,36 +10,36 @@ class Api::V1::BooksController < ApplicationController
   end
 
 
-    private 
-    def edge_case_response
-      return render json: ErrorSerializer.format_error(error_messages[:missing_params]), status: 400 if missing_params 
-      return render json: ErrorSerializer.format_error(error_messages[:empty_params]), status: 400 if empty_params 
-      return render json: ErrorSerializer.format_error(error_messages[:improper_quantity]), status:400 if improper_quantity 
-    end
+  private 
+  def edge_case_response
+    return render json: ErrorSerializer.format_error(error_messages[:missing_params]), status: 400 if missing_params 
+    return render json: ErrorSerializer.format_error(error_messages[:empty_params]), status: 400 if empty_params 
+    return render json: ErrorSerializer.format_error(error_messages[:improper_quantity]), status:400 if improper_quantity 
+  end
 
-    def edge_case_conditions
-      missing_params || empty_params || improper_quantity
-    end
+  def edge_case_conditions
+    missing_params || empty_params || improper_quantity
+  end
 
-    def missing_params
-      !params.has_key?(:location) || !params.has_key?(:quantity)
-    end
+  def missing_params
+    !params.has_key?(:location) || !params.has_key?(:quantity)
+  end
 
-    def improper_quantity
-      params[:quantity].to_i <= 0 
-    end
+  def improper_quantity
+    params[:quantity].to_i <= 0 
+  end
 
-    def empty_params
-      params.values.include?('')
-    end
+  def empty_params
+    params.values.include?('')
+  end
 
-    def error_messages 
-      {
-        missing_params: 'Both location and quantity parameters are required',
-        empty_params: 'Location and quantity parameters cannot be empty',
-        improper_quantity: 'Quantity parameter must be a positive integer'
-      }
-    end
+  def error_messages 
+    {
+      missing_params: 'Both location and quantity parameters are required',
+      empty_params: 'Location and quantity parameters cannot be empty',
+      improper_quantity: 'Quantity parameter must be a positive integer'
+    }
+  end
 end
 
 
