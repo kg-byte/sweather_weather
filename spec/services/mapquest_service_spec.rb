6c3be6cf997f3556860558bcc955a75cb752bed0
Kim G <xiaole.guo@yahoo.com>
2022-06-13 17:51:57 -0600

    add get_route class method and test in mapquest service

--- spec/services/mapquest_service_spec.rb
+++ spec/services/mapquest_service_spec.rb
@@ -12,4 +12,19 @@ RSpec.describe MapquestService do
     expect(data[:results][0][:locations][0]).to have_key(:latLng)
     expect(data[:results][0][:locations][0][:latLng]).to eq({ lat: 39.738453, lng: -104.984853 })
   end
+
+  it 'returns route data', :vcr do
+    json = {locations: ['denver,co', 'los angeles,ca']}.to_json
+    data = MapquestService.get_route(json)
+
+    expect(data).to be_a(Hash)
+    expect(data[:route]).to be_a(Hash)
+    expect(data[:route][:boundingBox]).to be_a(Hash)
+    expect(data[:route][:boundingBox]).to have_key(:ul)
+    expect(data[:route][:boundingBox][:ul]).to have_key(:lng)
+    expect(data[:route][:boundingBox][:ul]).to have_key(:lat)
+    expect(data[:route][:formattedTime]).to be_a(String)
+  end
+
+
 end
