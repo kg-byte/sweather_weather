f860448a003ef1c6a8d38cbf4c90a1f41a1ac65d
Kim G <xiaole.guo@yahoo.com>
2022-06-13 16:05:51 -0600

    add validations tests in user model test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   has_many :api_keys, as: :bearer, dependent: :destroy
+  validates :email, presence: true, uniqueness: true
+  validates_presence_of :password_digest, require: true
   has_secure_password
 end
