6cfd22abf89271901b811d8308926327098c051d
Kim G <xiaole.guo@yahoo.com>
2022-06-13 22:17:27 -0600

    refactor books controller

--- app/controllers/api/v1/trips_controller.rb
+++ app/controllers/api/v1/trips_controller.rb
@@ -6,7 +6,10 @@ class Api::V1::TripsController < ApplicationController
 
   def index
     edge_case_response if edge_case_conditions
-    serialize_trip unless edge_case_conditions
+    unless edge_case_conditions
+      serialize_valid_trip(trip) if trip.instance_of?(Trip)
+      serialize_impossible_route(trip) if trip.instance_of?(ImpossibleRoute)
+    end
   end
 
   private
@@ -15,15 +18,19 @@ class Api::V1::TripsController < ApplicationController
     params.permit(:origin, :destination, :api_key)
   end
 
-  def serialize_trip
-    trip = MapquestFacade.get_route(trip_params[:origin], trip_params[:destination])
-    serialize_valid_trip(trip) if trip.instance_of?(Trip)
-    render json: TripSerializer.format_trip(trip), status: :ok if trip.instance_of?(ImpossibleRoute)
+  def trip
+    MapquestFacade.get_route(trip_params[:origin], trip_params[:destination])
+  end
+
+  def eta_weather
+    OpenweatherFacade.get_weather_at_eta(trip.destination_geocode, trip.eta_in_hours)
   end
 
   def serialize_valid_trip(trip)
-    eta_weather = OpenweatherFacade.get_weather_at_eta(trip.destination_geocode, trip.eta_in_hours)
     render json: TripSerializer.format_trip(trip, eta_weather), status: :ok
   end
 
+  def serialize_impossible_route(trip)
+    render json: TripSerializer.format_trip(trip), status: :ok 
+  end
 end
