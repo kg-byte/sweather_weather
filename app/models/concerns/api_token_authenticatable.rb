90b12c64479d8a8f225f3d59ccd41ab534b95407
Kim G <xiaole.guo@yahoo.com>
2022-06-13 21:18:49 -0600

    add api token authenticatable module to authenticate api key passed in body

---
+++ app/models/concerns/api_token_authenticatable.rb
@@ -0,0 +1,15 @@
+module ApiTokenAuthenticatable
+
+  def authenticate_with_api_key(api_key)
+    if !authenticate(api_key)
+      error = 'Invalid API token!'
+      render json: ErrorSerializer.format_error(error), status: :unauthorized
+    end
+  end
+
+
+private 
+  def authenticate(api_key)
+    ApiKey.find_by token: api_key 
+  end
+end
