82c46627177523d934c68b6e3af558e25918d90c
Kim G <xiaole.guo@yahoo.com>
2022-06-13 20:56:14 -0600

    add and test impossible route poro

---
+++ app/poros/impossible_route.rb
@@ -0,0 +1,8 @@
+class ImpossibleRoute
+attr_reader :start_city, :end_city, :travel_time
+  def initialize(origin, destination)
+	@start_city = origin
+	@end_city = destination
+	@travel_time = 'impossible route'
+  end
+end
