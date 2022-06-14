a95cd6f7ef28d7d3c689d6acbbf746ff4f506951
Kim G <xiaole.guo@yahoo.com>
2022-06-13 22:51:40 -0600

    refactor users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,10 +4,19 @@ class Api::V1::UsersController < ApplicationController
     user = User.create(downcase_email_params)
     if user.save
       api_key = user.api_keys.create! token: SecureRandom.hex
-      render json: UserSerializer.format_user(user, api_key), status: :created
+      serialize_user(user, api_key)
     else
       error = user.errors.full_messages.to_sentence.to_s
-      render json: ErrorSerializer.format_error(error), status: :unauthorized
+      serialize_error(error)
     end
+  end
+
+  private
+  def serialize_user(user, api_key)
+    render json: UserSerializer.format_user(user, api_key), status: :created
+  end
+
+  def serialize_error(error)
+    render json: ErrorSerializer.format_error(error), status: :unauthorized
   end
 end
