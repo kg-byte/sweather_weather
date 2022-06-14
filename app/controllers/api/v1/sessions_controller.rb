f700c334c07028d4536c691d82ec6b003a5e2568
Kim G <xiaole.guo@yahoo.com>
2022-06-13 22:25:09 -0600

    refactor sessions controller

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,12 +1,31 @@
 class Api::V1::SessionsController < ApplicationController
   def create
-    user = User.find_by(email: params[:email])
-    if user && user.authenticate(params[:password])
-      api_key = user.api_keys.first
-      render json: UserSerializer.format_user(user, api_key), status: :ok
-    else
-      error = 'Incorrect Credentials. Please try again!'
-      render json: ErrorSerializer.format_error(error), status: :unauthorized
-    end
+    serialize_user if authenticated
+    serialize_error unless authenticated
+  end
+
+  private
+  def authenticated
+    user && user.authenticate(params[:password])
+  end
+
+  def user
+    User.find_by(email: params[:email])
+  end
+
+  def api_key
+    user.api_keys.first
+  end
+
+  def error
+    'Incorrect Credentials. Please try again!'
+  end
+
+  def serialize_user
+    render json: UserSerializer.format_user(user, api_key), status: :ok
+  end
+
+  def serialize_error
+    render json: ErrorSerializer.format_error(error), status: :unauthorized
   end
 end
