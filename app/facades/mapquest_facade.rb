9b7d3bd27e1dd281f8501172fe89b0598d7262cb
Kim G <xiaole.guo@yahoo.com>
2022-06-13 18:13:32 -0600

    add get_route method and tests in mapquest facade to return a trip poro

--- app/facades/mapquest_facade.rb
+++ app/facades/mapquest_facade.rb
@@ -2,4 +2,10 @@ class MapquestFacade
   def self.get_geocode(location)
     MapquestService.get_geocode(location)[:results][0][:locations].first[:latLng]
   end
+
+  def self.get_route(origin, destination)
+    params = {locations: [origin, destination]}.to_json
+    route_data = MapquestService.get_route(params)
+    Trip.new(origin, destination, route_data)
+  end
 end
--- spec/facades/mapquest_facade_spec.rb
+++ spec/facades/mapquest_facade_spec.rb
@@ -3,12 +3,26 @@ require 'rails_helper'
 RSpec.describe MapquestFacade do
   it 'returns a hash of geocode' do
     data = JSON.parse(File.read('spec/fixtures/mapquest_response.json'), symbolize_names: true)
-
     allow(MapquestService).to receive(:get_geocode).and_return(data)
 
     geocode = MapquestFacade.get_geocode('denver,co')
     expect(geocode).to be_a(Hash)
     expect(geocode[:lat]).to eq(39.738453)
     expect(geocode[:lng]).to eq(-104.984853)
+  end
+
+  it 'returns route data' do
+    data = JSON.parse(File.read('spec/fixtures/mapquest_route.json'), symbolize_names: true)
+
+    allow(MapquestService).to receive(:get_route).and_return(data)
+
+    trip = MapquestFacade.get_route('denver,co', 'los angeles,ca')
+
+    expect(trip).to be_a(Trip)
+    expect(trip.start_city).to eq('denver,co')
+    expect(trip.end_city).to eq('los angeles,ca')
+    expect(trip.travel_time).to eq('40 hours, 16 minutes')
+    expect(trip.destination_geocode).to eq({:lat=>41.596916, :lng=>-118.244469})  
+
   end
 end
